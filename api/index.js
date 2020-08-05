c8fdd1ab4ed18fb2b0f1b9f76633ea08f3e7a0f9
resparzasoto <rubenesparzasoto95@gmail.com>
2020-08-04 22:22:34 -0600

    Estructura inicial del proyecto: API y rutas

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const chalk = require('chalk');
+
+const config = require('../config');
+const user = require('./components/user/network');
+
+const app = express();
+
+//ROUTER
+app.use('/api/user', user);
+
+app.listen(config.api.port, () => {
+    console.log(chalk.green('API escuchando en el puerto', chalk.yellow(config.api.port)) );;
+});
---
+++ network/response.js
@@ -0,0 +1,20 @@
+const success = (req, res, message = 'Done', status = 200) => {
+    res.status(status).send({
+        error: false,
+        status: status,
+        body: message,
+    });
+}
+
+const error = (req, res, message = 'Internal Server Error', status = 500) => {
+    res.status(status).send({
+        error: true,
+        status: status,
+        body: message,
+    });
+}
+
+module.exports = {
+    success,
+    error,
+};
