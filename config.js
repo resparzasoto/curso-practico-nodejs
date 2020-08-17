75c49bb43eb5a86a9577cb8c4b837c3b10175a86
resparzasoto <rubenesparzasoto95@gmail.com>
2020-08-16 18:47:54 -0600

    Gestión de microservicios con PM2

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 module.exports = {
     api: {
-        port: process.env.PORT,
-        environment: process.env.NODE_ENV,
+        port: process.env.PORT || 3000,
+        environment: process.env.NODE_ENV || "development",
     },
     post: {
-        port: process.env.POST_PORT,
+        port: process.env.POST_PORT || 3002,
     },
     jwt: {
-        secret: process.env.SECRET,
+        secret: process.env.SECRET || "s3cr3t-phr4s3d",
     },
     mssql: {
-        server: process.env.MSSQL_SERVER,
-        port: Number(process.env.MSSQL_PORT),
-        user: process.env.MSSQL_USER,
-        password: process.env.MSSQL_PASSWORD,
-        database: process.env.MSSQL_DATABASE,
+        server: process.env.MSSQL_SERVER || "RUBENMSI",
+        port: Number(process.env.MSSQL_PORT) || 1433,
+        user: process.env.MSSQL_USER || "sa",
+        password: process.env.MSSQL_PASSWORD || "Admin.123456",
+        database: process.env.MSSQL_DATABASE || "platzisocial",
         options: {
-            encrypted: process.env.MSSQL_OPTIONS_ENCRYPTED,
-            enableArithAbort: Boolean(process.env.MSSQL_OPTIONS_ENABLE_ARITH_ABORT),
+            encrypted: process.env.MSSQL_OPTIONS_ENCRYPTED || true,
+            enableArithAbort: Boolean(process.env.MSSQL_OPTIONS_ENABLE_ARITH_ABORT) || true,
         },
     },
     mssqlService: {
-        host: process.env.MSSQL_SERVICE_HOST,
-        port: process.env.MSSQL_SERVICE_PORT,
+        host: process.env.MSSQL_SERVICE_HOST || "localhost",
+        port: process.env.MSSQL_SERVICE_PORT || 3001,
     },
 };
